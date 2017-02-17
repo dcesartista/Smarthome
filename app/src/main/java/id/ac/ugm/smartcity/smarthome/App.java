71ce88646f862fe9c4bed05e4e4d88acbd9801d0
dcesartista <m.cesartista@gmail.com>
2017-02-17 09:59:47 +0700

    add App class

--- app/src/main/java/id/ac/ugm/smartcity/smarthome/App.java
+++ app/src/main/java/id/ac/ugm/smartcity/smarthome/App.java
@@ -1,8 +1,11 @@
 package id.ac.ugm.smartcity.smarthome;
 
+import android.app.Application;
+
 /**
  * Created by dito on 17/02/17.
  */
 
-public class App {
+public class App extends Application {
+
 }
